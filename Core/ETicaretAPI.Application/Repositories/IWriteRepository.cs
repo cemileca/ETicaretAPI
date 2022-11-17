d74c9502fe4aa3c75d7d11fae0b75eddc00efe67
Hatice Cemile <97739544+cemileca@users.noreply.github.com>
2022-11-17 23:49:44 +0300

    Update

---
+++ Core/ETicaretAPI.Application/Repositories/IWriteRepository.cs
@@ -0,0 +1,21 @@
+﻿using ETicaretAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Repositories
+{
+    public interface IWriteRepository<T>:IRepository<T> where T : BaseEntity
+    {
+        Task<bool> AddAsync(T entity);
+        Task<bool> AddRangeAsync(List<T> data);
+        bool Remove(T model);
+        bool RemoveRange(List<T> data);
+        Task<bool> RemoveById(string Id);
+        bool Update(T model);
+
+        Task<int> SaveAsync();
+    }
+}
---
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿
+
+using ETicaretAPI.Application.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretAPI.API.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        readonly private IProductReadRepository _productReadRepository;
+        readonly private IProductWriteRepository _productWriteRepository;
+        public ProductsController(IProductReadRepository productReadRepository, IProductWriteRepository productWriteRepository)
+        {
+            _productReadRepository = productReadRepository;
+            _productWriteRepository = productWriteRepository;
+        }
+        [HttpGet]
+        public async void Get()
+        {
+            await _productWriteRepository.AddRangeAsync(new()
+            {
+                new(){Id=Guid.NewGuid(), CreateDate=DateTime.UtcNow, Name="Product 7",  Price=125, Stock=658},
+                new(){Id=Guid.NewGuid(), CreateDate=DateTime.UtcNow, Name="Product 8",  Price=15, Stock=600},
+                new(){Id=Guid.NewGuid(), CreateDate=DateTime.UtcNow, Name="Product 9",  Price=185, Stock=450},
+            });
+            var count = await _productWriteRepository.SaveAsync();
+        }
+    }
+}
